e9464996e50a0984a6ac135229e9e45bb20d9be6
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-10-23 20:32:40 +0600

    bard get

--- index.js
+++ index.js
@@ -9,7 +9,31 @@ const port = process.env.PORT || 3000; // Use process.env.PORT or default to 300
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zkhcmhi.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db("productDB").collection("Product")
+
+
+        // code 2 get all product
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
 
 
